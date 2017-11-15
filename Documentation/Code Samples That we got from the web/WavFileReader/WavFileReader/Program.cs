148128fdbe01957108ded8f10148b6fea4da17d0
slawinskam <m.slawinska@student.mini.pw.edu.pl>
2017-11-15 12:32:11 +0100

    face feature extraction not working

--- Documentation/Code Samples That we got from the web/WavFileReader/WavFileReader/Program.cs
+++ Documentation/Code Samples That we got from the web/WavFileReader/WavFileReader/Program.cs
@@ -1,121 +1,30 @@
 ﻿using SpeechRecognition;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FaceRecognition;
 
 namespace WavFileReader
 {
     class Program
     {
-        public class WavHeader
-        {
-            public byte[] riffID; //not needed
-            public uint size;  
-            public byte[] wavID;  //not needed
-            public byte[] fmtID;  //not needed
-            public uint fmtSize; 
-            public ushort format; //if 1 then uncompressed, anything else -> compressed
-            public ushort channels; //mono/stereo
-            public uint sampleRate; //https://pl.wikipedia.org/wiki/Pr%C3%B3bkowanie
-            public uint bytePerSec; //SampleRate * NumChannels * BitsPerSample/8
-            public ushort blockSize; // NumChannels* BitsPerSample/8 = number of bytes per one sample for all channels
-            public ushort bit;  
-            public byte[] dataID; //not needed
-            public uint dataSize; // == NumSamples * NumChannels * BitsPerSample/8 
-
-            public override string ToString()
-            {
-                Console.WriteLine("RiffId " + riffID[0] + " " +
-                    riffID[1] + " " + riffID[2] + " " + riffID[3]);
-                Console.WriteLine("Size " + size);
-                Console.WriteLine("WavId " + wavID[0] + " " +
-                    wavID[1] + " " + wavID[2] + " " + wavID[3]);
-                Console.WriteLine("FmtId " + fmtID[0] + " " +
-                    fmtID[1] + " " + fmtID[2] + " " + fmtID[3]);
-                Console.WriteLine("FmtSize " + fmtSize);
-                Console.WriteLine("Format " + format);
-                Console.WriteLine("Channels " + channels);
-                Console.WriteLine("SampleRate " + sampleRate);
-                Console.WriteLine("Bytes per second " + bytePerSec);
-                Console.WriteLine("Block size " + blockSize);
-                Console.WriteLine("Bit " + bit);
-                Console.WriteLine("DataId " + dataID[0] + " " +
-                    dataID[1] + " " + dataID[2] + " " + dataID[3]);
-                Console.WriteLine("Data size " + dataSize);
-                return null;
-            }
-        }
-
         static void Main(string[] args)
         {
-            WavHeader Header = new WavHeader();
-            List<short> lDataList = new List<short>();
-            List<short> rDataList = new List<short>();
-            string path = "../../Samples/0_0_0_0_1_1_1_1.wav";
-            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
-            using (BinaryReader br = new BinaryReader(fs))
-            {
-                try
-                {
-                    Header.riffID = br.ReadBytes(4);
-                    Header.size = br.ReadUInt32();
-                    Header.wavID = br.ReadBytes(4);
-                    Header.fmtID = br.ReadBytes(4);
-                    Header.fmtSize = br.ReadUInt32();
-                    Header.format = br.ReadUInt16();
-                    Header.channels = br.ReadUInt16();
-                    Header.sampleRate = br.ReadUInt32();
-                    Header.bytePerSec = br.ReadUInt32();
-                    Header.blockSize = br.ReadUInt16();
-                    Header.bit = br.ReadUInt16();
-                    Header.dataID = br.ReadBytes(4);
-                    Header.dataSize = br.ReadUInt32();
-                    int index = 0;
-                    //1 channel --> add to 1 list, 2 channels --> add to 2 lists
-                    for (int i = 0; i < Header.dataSize / Header.blockSize; i++)
-                    {
-                        lDataList.Add((short)br.ReadUInt16());
-                        index = i;
-                    }
-                }
-                finally
-                {
-                    if (br != null)
-                    {
-                        br.Close();
-                    }
-                    if (fs != null)
-                    {
-                        fs.Close();
-                    }
-                }
-            }
-            Console.WriteLine(Header);
+            Bitmap b = new Bitmap("pict.bmp");
 
-            FrameMaker frameMaker = new FrameMaker(frameLength: 0.05f, frameInterval: 0.025f);
-            var frame = frameMaker.ToFrames(lDataList, (int)Header.sampleRate);
-            Frame sampleCopy = new Frame(frame[120].Samples);
-            Window windowFunction = new GaussWindow();
-            
-            for (int i = 0; i < frame.Count; i++)
+            double[] lambda = {4.0/3, 2.0/3 };
+            GaborFilter gf = new GaborFilter(2, 1, lambda, Math.PI/2, 5);
+
+            List<Bitmap> lb = gf.ApplyFilter(b);
+
+            for(int u=0; u<lb.Count; u++)
             {
-                windowFunction.ApplyWindow(frame[i]);
+                lb[u].Save("img" + u + ".bmp");
             }
-            Frame sampleCheck = new Frame(frame[120].Samples);
-            foreach (var sample in frame)
-            {
-                foreach (var f in sample.Samples)
-                {
-                    if (f > 0)
-                    {
-                        Console.WriteLine("f =" + f);
-                    }
-                }
-            }
-            return;
         }
     }
 }
---
+++ Project/BiometricFusionSystem/FaceRecognition/GaborFilter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceRecognition
+{
+    class GaborFilter
+    {
+        private double stdDevX;
+        private double stdDevY;
+        private double[] lambda;
+        private double phase;
+        private int n;
+
+        public GaborFilter(double _stdDevX, double _stdDevY, double[] _lambda, double _phase, int _n)
+        {
+            stdDevX = _stdDevX;
+            stdDevY = _stdDevY;
+            lambda = _lambda;
+            phase = _phase;
+            n = _n;
+        }
+
+
+        public Bitmap CalculateGaborFilter(Bitmap b, int k, double lambda)
+        {
+            Bitmap bmp = new Bitmap(b.Width, b.Height);
+            for (int i = 0; i < b.Width; i++)
+            {
+                for (int j = 0; j < b.Height; j++)
+                {
+                    double xThetak = i * Math.Cos(k*Math.PI/n) + j * Math.Sin(k * Math.PI / n);
+                    double yThetak = j * Math.Cos(k * Math.PI / n) - i * Math.Sin(k * Math.PI / n);
+
+                    double exp = -1 * (Math.Pow(xThetak, 2) / Math.Pow(stdDevX, 2) + Math.Pow(yThetak, 2) / Math.Pow(stdDevY, 2));
+                    double cos = 2 * Math.PI * 1 / lambda + phase;
+                    double gabor = Math.Exp(exp) * Math.Cos(cos);
+                    Color c = Color.FromArgb((int)gabor);
+                    bmp.SetPixel(i, j, c);
+                }
+            }
+            return bmp;
+        }
+
+        public List<Bitmap> ApplyFilter(Bitmap b)
+        {
+            List<Bitmap> lb = new List<Bitmap>();
+            for (int l = 0; l < lambda.Count(); l++)
+            {
+                for (int k = 1; k <= n; k++)
+                {
+                    lb.Add(CalculateGaborFilter(b, k, l));
+                }
+            }
+            return lb;
+        }
+
+    }
+}
