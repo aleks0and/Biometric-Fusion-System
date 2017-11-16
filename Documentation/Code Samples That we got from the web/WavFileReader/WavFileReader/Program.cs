9ed10ba57369e6f6a2cecf561b4f1bf37c14ae9a
Kornel Żaba <kornelzaba@gmail.com>
2017-11-16 17:51:30 +0100

    Gabor filter is now a convolution filter

--- Documentation/Code Samples That we got from the web/WavFileReader/WavFileReader/Program.cs
+++ Documentation/Code Samples That we got from the web/WavFileReader/WavFileReader/Program.cs
@@ -14,10 +14,10 @@ namespace WavFileReader
     {
         static void Main(string[] args)
         {
-            Bitmap b = new Bitmap("pict.bmp");
+            Bitmap b = new Bitmap(@"../../im-grayscale.bmp");
 
-            double[] lambda = {4.0/3, 2.0/3 };
-            GaborFilter gf = new GaborFilter(2, 1, lambda, Math.PI/2, 5);
+            double[] lambda = { 10, 5 };
+            GaborFilter gf = new GaborFilter(2, 1, lambda, Math.PI/2, 4, 3);
 
             List<Bitmap> lb = gf.ApplyFilter(b);
 
@@ -25,6 +25,25 @@ namespace WavFileReader
             {
                 lb[u].Save("img" + u + ".bmp");
             }
+
+            var superpos = new Bitmap(lb[0].Width, lb[0].Height);
+            for(int i = 0; i < superpos.Width; i++)
+            {
+                for(int j = 0; j < superpos.Height; j++)
+                {
+                    int min = 255;
+                    for(int k = 0; k < lb.Count; k++)
+                    {
+                        var px = lb[k].GetPixel(i, j);
+                        if(px.R < min)
+                        {
+                            min = px.R;
+                        }
+                    }
+                    superpos.SetPixel(i, j, Color.FromArgb(min, min, min));
+                }
+            }
+            superpos.Save("super.bmp");
         }
     }
 }
--- Project/BiometricFusionSystem/FaceRecognition/GaborFilter.cs
+++ Project/BiometricFusionSystem/FaceRecognition/GaborFilter.cs
@@ -9,54 +9,106 @@ namespace FaceRecognition
 {
     class GaborFilter
     {
-        private double stdDevX;
-        private double stdDevY;
-        private double[] lambda;
-        private double phase;
-        private int n;
+        private double _stdDevX;
+        private double _stdDevY;
+        private double[] _lambda;
+        private double _phase;
+        private int _n;
+        private int _kernelSize;
 
-        public GaborFilter(double _stdDevX, double _stdDevY, double[] _lambda, double _phase, int _n)
+        public GaborFilter(double stdDevX, double stdDevY, double[] lambda, double phase, int n, int kernelSize)
         {
-            stdDevX = _stdDevX;
-            stdDevY = _stdDevY;
-            lambda = _lambda;
-            phase = _phase;
-            n = _n;
+            _stdDevX = stdDevX;
+            _stdDevY = stdDevY;
+            _lambda = lambda;
+            _phase = phase;
+            _n = n;
+            _kernelSize = kernelSize;
         }
 
 
         public Bitmap CalculateGaborFilter(Bitmap b, int k, double lambda)
         {
-            Bitmap bmp = new Bitmap(b.Width, b.Height);
-            for (int i = 0; i < b.Width; i++)
-            {
-                for (int j = 0; j < b.Height; j++)
-                {
-                    double xThetak = i * Math.Cos(k*Math.PI/n) + j * Math.Sin(k * Math.PI / n);
-                    double yThetak = j * Math.Cos(k * Math.PI / n) - i * Math.Sin(k * Math.PI / n);
+            int max = _kernelSize - 1;
+            Bitmap bmp = new Bitmap(b.Width - max, b.Height - max);
+            var kernel = GetKernel(k, lambda);
 
-                    double exp = -1 * (Math.Pow(xThetak, 2) / Math.Pow(stdDevX, 2) + Math.Pow(yThetak, 2) / Math.Pow(stdDevY, 2));
-                    double cos = 2 * Math.PI * 1 / lambda + phase;
-                    double gabor = Math.Exp(exp) * Math.Cos(cos);
-                    Color c = Color.FromArgb((int)gabor);
-                    bmp.SetPixel(i, j, c);
+            for(int i = max / 2; i < b.Width - max / 2; i++)
+            {
+                for(int j = max / 2; j < b.Height - max / 2; j++)
+                {
+                    ApplyKernel(b, bmp, i, j, kernel);
                 }
             }
             return bmp;
         }
 
+        public double Clamp(double c)
+        {
+            if (c > 255)
+                return 255;
+            else if (c < 0)
+                return 0;
+            else
+                return c;
+        }
+
         public List<Bitmap> ApplyFilter(Bitmap b)
         {
             List<Bitmap> lb = new List<Bitmap>();
-            for (int l = 0; l < lambda.Count(); l++)
+            for (int l = 0; l < _lambda.Count(); l++)
             {
-                for (int k = 1; k <= n; k++)
+                for (int k = 1; k <= _n; k++)
                 {
-                    lb.Add(CalculateGaborFilter(b, k, l));
+                    lb.Add(CalculateGaborFilter(b, k, _lambda[l]));
                 }
             }
             return lb;
         }
 
+        private double GetGaborValue(int x, int y, int angle, double waveLength)
+        {
+            double xTheta = x * Math.Cos(angle * Math.PI / _n) + y * Math.Sin(angle * Math.PI / _n);
+            double yTheta = y * Math.Cos(angle * Math.PI / _n) - x * Math.Sin(angle * Math.PI / _n);
+            double exp = -(Math.Pow(xTheta, 2) / Math.Pow(_stdDevX, 2) + Math.Pow(yTheta, 2) / Math.Pow(_stdDevY, 2));
+            double cos = 2 * Math.PI * (xTheta / waveLength) + _phase;
+
+            return Math.Exp(exp) * Math.Cos(cos);
+        }
+
+        private double[,] GetKernel(int angle, double waveLength)
+        {
+            int max = (_kernelSize - 1) / 2;
+            var kernel = new double[_kernelSize, _kernelSize];
+
+            double sum = 0;
+            for(int x = -max; x <= max; x++)
+            {
+                for(int y = -max; y <= max; y++)
+                {
+                    kernel[x + max, y + max] = GetGaborValue(x, y, angle, waveLength);
+                    sum += kernel[x + max, y + max];
+                }
+            }
+
+            return kernel;
+        }
+
+        private void ApplyKernel(Bitmap src, Bitmap dest, int x, int y, double[,] kernel)
+        {
+            int max = (_kernelSize - 1) / 2;
+            double sumR = 0, sumG = 0, sumB = 0;
+            for (int i = -max; i <= max; i++)
+            {
+                for (int j = -max; j <= max; j++)
+                {
+                    var srcColor = src.GetPixel(x + i, y + j);
+                    sumR += kernel[i + max, j + max] * srcColor.R;
+                    sumG += kernel[i + max, j + max] * srcColor.G;
+                    sumB += kernel[i + max, j + max] * srcColor.B;
+                }
+            }
+            dest.SetPixel(x - max, y - max, Color.FromArgb(255 - (int)Clamp(sumR), 255 - (int)Clamp(sumG), 255 - (int)Clamp(sumB)));
+        }
     }
 }
