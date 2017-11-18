ed7bdc12f89c99da7b9e6786a431583057552b5f
Kornel Żaba <kornelzaba@gmail.com>
2017-11-18 21:56:27 +0100

    Added FFT for speech and periodogram estimate calculation

---
+++ Project/BiometricFusionSystem/SpeechRecognition/Periodogram.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechRecognition
+{
+    public class Periodogram
+    {
+
+        public List<double> GetEstimate(Frame frame)
+        {
+            var fourierCoeffs = FourierTransform.Apply(frame.Samples);
+
+            var estimates = new List<double>();
+            for(int i = 0; i < fourierCoeffs.Length; i++)
+            {
+                var powerSpectrum = Math.Pow(fourierCoeffs[i].Magnitude, 2) / frame.Samples.Count;
+                estimates.Add(powerSpectrum);
+            }
+            return estimates;
+        }
+    }
+}
