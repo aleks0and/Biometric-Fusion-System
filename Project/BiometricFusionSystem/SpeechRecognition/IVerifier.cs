a0c551d314296b3e62af14de2c020925c6f2b245
Kornel Żaba <kornelzaba@gmail.com>
2017-11-21 17:21:15 +0100

    Dynamic time warping done

---
+++ Project/BiometricFusionSystem/SpeechRecognition/IVerifier.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SpeechRecognition
+{
+    public interface IVerifier
+    {
+        bool Verify(List<double> input, List<double> template);
+    }
+}
