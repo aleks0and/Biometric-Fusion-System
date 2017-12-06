8b88f1e2410784f5ccc926ce622b03e5753697a9
Kornel Żaba <kornelzaba@gmail.com>
2017-12-06 18:16:56 +0100

    Added photo/recording acquistion commands

--- Project/BiometricFusionSystem/Camera/Ffmpeg.cs
+++ Project/BiometricFusionSystem/Camera/Ffmpeg.cs
@@ -11,37 +11,55 @@ namespace Camera
 {
     public class Ffmpeg
     {
+        private string _cameraName = "Logitech HD Webcam C270";
+        private string _microphoneName = "Mikrofon(HD Webcam C270)";
+        private Process _process;
+        private EventHandler _handler;
         //handle exited event on process to know when it ends
 
-        public static void TakePicture()
+        public void TakePicture(string outputPath, EventHandler handler)
         {
-            Process process = new Process();
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.RedirectStandardError = true;
-            process.StartInfo.FileName = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) +
+            _handler = handler;
+            _process = new Process();
+            _process.StartInfo.RedirectStandardOutput = true;
+            _process.StartInfo.RedirectStandardError = true;
+            _process.StartInfo.FileName = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) +
                                     @"\bin\ffmpeg.exe";
 
-            process.StartInfo.Arguments = "-f dshow -i video=\"TOSHIBA Web Camera - HD\" -qscale:v 2 -vframes 1 -s 1280x960 -y \"C:\\Users\\Martyna\\Desktop\\pic.bmp\"";
+            _process.StartInfo.Arguments = "-f dshow -i video=\"" + _cameraName + "\" -qscale:v 2 -vframes 1 -s 1280x960 -y \"" + outputPath + "\"";
 
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.CreateNoWindow = true;
-            process.Start();
+            _process.StartInfo.UseShellExecute = false;
+            _process.StartInfo.CreateNoWindow = true;
+            _process.EnableRaisingEvents = true;
+            _process.Exited += _handler;
+            _process.Start();
+            
         }
 
-        public static void RecordAudio()
+        public void RecordAudio(string outputPath, EventHandler handler)
         {
-            Process process2 = new Process();
-            process2.StartInfo.RedirectStandardOutput = true;
-            process2.StartInfo.RedirectStandardError = true;
-            process2.StartInfo.FileName = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) +
+            _handler = handler;
+            _process = new Process();
+            _process.StartInfo.RedirectStandardOutput = true;
+            _process.StartInfo.RedirectStandardError = true;
+            _process.StartInfo.FileName = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) +
                                     @"\bin\ffmpeg.exe";
 
-            process2.StartInfo.Arguments = "-f dshow -i audio=\"Internal Mic(IDT High Definition Audio CODEC)\" -t 10 \"C: \\Users\\Martyna\\Desktop\\rec1.wav\"";
+            _process.StartInfo.Arguments = "-f dshow -i audio=\"" + _microphoneName + "\" -t 10 \"" + outputPath + "\"";
 
-            process2.StartInfo.UseShellExecute = false;
-            process2.StartInfo.CreateNoWindow = true;
-            process2.Start();
+            _process.StartInfo.UseShellExecute = false;
+            _process.StartInfo.CreateNoWindow = true;
+            _process.EnableRaisingEvents = true;
+            _process.Exited += _handler;
+            _process.Start();
             Thread.Sleep(12000);
+        }
+
+        public void EndEvent()
+        {
+            _process.Exited -= _handler;
+            _process = null;
+            _handler = null;
         }
     }
 }
--- Project/BiometricFusionSystem/Gui/ViewModel/MainViewModel.cs
+++ Project/BiometricFusionSystem/Gui/ViewModel/MainViewModel.cs
@@ -1,24 +1,54 @@
-﻿using Gui.Model;
+﻿using Camera;
+using Gui.Model;
 using Gui.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Gui.ViewModel
 {
     public class MainViewModel : BindableBase
     {
+        private Ffmpeg _ffmpeg;
         private WindowService _service = new WindowService();
         public ICommand OpenOptionsCommand { get; set; }
         public ICommand OpenVerificationCommand { get; set; }
+        public ICommand AcquirePhotoCommand { get; set; }
+        public ICommand AcquireRecordingCommand { get; set; }
         
         public MainViewModel()
         {
             OpenOptionsCommand = new RelayCommand(OpenOptions, canExecute => true);
             OpenVerificationCommand = new RelayCommand(OpenVerification, canExecute => true);
+            AcquirePhotoCommand = new RelayCommand(AcquirePhoto, canExecute => true);
+            AcquireRecordingCommand = new RelayCommand(AcquireRecording, canExecute => true);
+            _ffmpeg = new Ffmpeg();
+        }
+
+        private void AcquirePhoto(object parameter)
+        {
+            _ffmpeg.TakePicture("output.bmp", new EventHandler(AcquirePhotoHandler));
+        }
+
+        private void AcquireRecording(object parameter)
+        {
+            _ffmpeg.RecordAudio("output.wav", new EventHandler(AcquireRecordingHandler));
+        }
+
+        private void AcquirePhotoHandler(object parameter, EventArgs e)
+        { 
+            _ffmpeg.EndEvent();
+            MessageBox.Show("Photo acquired");
+
+        }
+        private void AcquireRecordingHandler(object parameter, EventArgs e)
+        {
+            _ffmpeg.EndEvent();
+            MessageBox.Show("Recording acquired");
         }
 
         private void OpenOptions(object parameter)
