496d28192b176553c1744e818824d9fb49a4d679
Kornel Żaba <kornelzaba@gmail.com>
2017-12-06 17:35:20 +0100

    Added MVVM utility classes

---
+++ Project/BiometricFusionSystem/Gui/Utility/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Gui.Utility
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        private Action<object> execute;
+        private Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> _execute, Predicate<object> _canExecute)
+        {
+            execute = _execute;
+            canExecute = _canExecute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
