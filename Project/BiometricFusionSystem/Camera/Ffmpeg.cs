270695c1e68411544c74dfa1214ebf5c235ce92f
Aleksander Kuśmierczyk <Aleksander.Kusmierczyk@gmail.com>
2017-12-06 18:52:46 +0100

    fixed acquire face and voice

--- Project/BiometricFusionSystem/Camera/Ffmpeg.cs
+++ Project/BiometricFusionSystem/Camera/Ffmpeg.cs
@@ -12,7 +12,7 @@ namespace Camera
     public class Ffmpeg
     {
         private string _cameraName = "Logitech HD Webcam C270";
-        private string _microphoneName = "Mikrofon(HD Webcam C270)";
+        private string _microphoneName = "Mikrofon (HD Webcam C270)";
         private Process _process;
         private EventHandler _handler;
         //handle exited event on process to know when it ends
@@ -26,7 +26,7 @@ namespace Camera
             _process.StartInfo.FileName = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) +
                                     @"\bin\ffmpeg.exe";
 
-            _process.StartInfo.Arguments = "-f dshow -i video=\"" + _cameraName + "\" -qscale:v 2 -vframes 1 -s 1280x960 -y \"" + outputPath + "\"";
+            _process.StartInfo.Arguments = "-f dshow -i video=\"" + _cameraName + "\" -qscale:v 0 -vframes 1 -s 640x480 -y \"" + outputPath + "\"";
 
             _process.StartInfo.UseShellExecute = false;
             _process.StartInfo.CreateNoWindow = true;
@@ -45,14 +45,14 @@ namespace Camera
             _process.StartInfo.FileName = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) +
                                     @"\bin\ffmpeg.exe";
 
-            _process.StartInfo.Arguments = "-f dshow -i audio=\"" + _microphoneName + "\" -t 10 \"" + outputPath + "\"";
+            _process.StartInfo.Arguments = "-f dshow -i audio=\"" + _microphoneName + "\" -ac 1 -ar 44100 -t 3 \"" + outputPath + "\"";
 
             _process.StartInfo.UseShellExecute = false;
             _process.StartInfo.CreateNoWindow = true;
             _process.EnableRaisingEvents = true;
             _process.Exited += _handler;
             _process.Start();
-            Thread.Sleep(12000);
+            
         }
 
         public void EndEvent()
