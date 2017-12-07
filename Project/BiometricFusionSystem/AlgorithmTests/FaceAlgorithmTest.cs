9991a6b6b29384b3eb7394d10b0b7c73bda31489
Aleksander Kuśmierczyk <Aleksander.Kusmierczyk@gmail.com>
2017-12-07 11:07:32 +0100

    tests for facial verification

--- Project/BiometricFusionSystem/AlgorithmTests/FaceAlgorithmTest.cs
+++ Project/BiometricFusionSystem/AlgorithmTests/FaceAlgorithmTest.cs
@@ -13,7 +13,9 @@ namespace AlgorithmTests
     [TestClass]
     public class FaceAlgorithmTest
     {
-        private string _sampleDirPath = @"../../Samples/";
+        // we need to change it so we keep the pictures in the DB instead of the project directory
+        private string _sampleDirPath = @"../../../../../../Samples/";
+        //private string _sampleDirPath = @"../../Samples/";
         private DynamicTimeWarping _timeWarping;
         private FaceFeatureExtractor _extractor;
         private int _finalMoment;
@@ -63,6 +65,30 @@ namespace AlgorithmTests
             //load images paths to listBmp, class names to classNames, and call LoadBitmaps(listBmp, classNames)
         }
 
+        [TestMethod]
+        public void ABADFemaleToFemaleSampleVerification()
+        {
+            TestMDC("AB", "AD");
+        }
+
+        [TestMethod]
+        public void APBCMaleToMaleSampleVerification()
+        {
+            TestMDC("AP", "BC");
+        }
+
+        [TestMethod]
+        public void DCLXMaleToMaleSampleVerification()
+        {
+            TestMDC("DC", "LX");
+        }
+
+        [TestMethod]
+        public void MSMS2MaleToFemaleSampleVerification()
+        {
+            TestMDC("MS", "MS2");
+        }
+
         private List<double> GetFeatures(string filePath)
         {
             string path = _sampleDirPath + filePath;
@@ -71,6 +97,38 @@ namespace AlgorithmTests
             bmp.Dispose();
             return features;
         }
+
+        private List<double> GetFeatures(string filePath, string directoryPath)
+        {
+            string path = directoryPath + filePath;
+            var bmp = new Bitmap(path);
+            var features = _extractor.GetFeatureVector(bmp);
+            bmp.Dispose();
+            return features;
+        }
+
+        private void TestMDC(string person1, string person2)
+        {
+            List<string> imagePaths = new List<string>();
+            List<string> classNames = new List<string>();
+            for (int i = 0; i < 3; i++)
+            {
+                classNames.Add(person1);
+                imagePaths.Add(person1 + "//" + person1 + (i + 1) + ".bmp");
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                classNames.Add(person2);
+                imagePaths.Add(person2 + "//" + person2 + (i + 1) + ".bmp");
+            }
+            var mdc = new MinimumDistanceClassifier();
+            LoadBitmaps(imagePaths, classNames, mdc);
+            var p1 = GetFeatures(person1 + "//" + person1 + "4.bmp");
+            var p2 = GetFeatures(person2 + "//" + person2 + "4.bmp");
+            Assert.AreEqual(person1, mdc.Classify(p1));
+            Assert.AreEqual(person2, mdc.Classify(p2));
+        }
+
         private List<double> GetGaborFeatures(string filePath)
         {
             string path = _sampleDirPath + filePath;
@@ -79,6 +137,7 @@ namespace AlgorithmTests
             bmp.Dispose();
             return features;
         }
+
         private void LoadBitmaps(List<string> listBmp, List<string> classNames, MinimumDistanceClassifier mdc)
         {
             List<List<double>> fvs = new List<List<double>>();
