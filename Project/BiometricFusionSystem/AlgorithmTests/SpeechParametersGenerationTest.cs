83e9f4ef6a614d243fcf56bc721d514df84ffbf6
slawinskam <m.slawinska@student.mini.pw.edu.pl>
2017-12-20 20:49:31 +0100

    face parameters generation tests done, results in the txt file

--- Project/BiometricFusionSystem/AlgorithmTests/SpeechParametersGenerationTest.cs
+++ Project/BiometricFusionSystem/AlgorithmTests/SpeechParametersGenerationTest.cs
@@ -14,40 +14,11 @@ using System.Drawing;
 namespace AlgorithmTests
 {
     [TestClass]
-    public class ParametersGenerationTest
+    public class SpeechParametersGenerationTest
     {
-        private string _sampleDirectory = @"C:\Users\aleks\Desktop\";
+        private string _sampleDirectory = @"C:\Users\Martyna\Desktop\";
         private string speechFilePath = @"..\..\..\..\..\Documentation\speechResult.txt";
-        private string faceFilePath = @"..\..\..\..\..\Documentation\faceResult.txt";
-
-        [TestMethod]
-        public void GenerateGaborFilterParemeters()
-        {
-            string[] datasets = new string[] { "data", "dataDecember" };
-            string directory = "faces";
-            double[] lambdamin = new double[] { };
-            double[] lambdamax = new double[] { };
-            int orientationMin = 4;
-            int orientationMax = 8;
-            double stdxMin = 0;
-            double stdxMax = 10; 
-            double bestAverage = 0;
-            Result result = new Result();
-
-            using (var writer = new StreamWriter(@"..\..\..\..\..\Documentation\bestFaceResult.txt"))
-            {
-                writer.WriteLine("Lambda: " + result.CoeffsLeft);
-                writer.WriteLine("Orientation count: " + result.FilterBanks);
-                writer.WriteLine("stdx: " + result.FrameLength);
-                writer.WriteLine("stdy: " + result.Window.GetType().FullName);
-                writer.WriteLine("Average: " + bestAverage.ToString("F2"));
-                for (int i = 0; i < result.Averages.Count; i++)
-                {
-                    writer.WriteLine("Average #{0}: {1}", i + 1, result.Averages[i].ToString("F2"));
-                }
-            }
-        }
-
+        
         [TestMethod]
         public void GenerateSpeechTests()
         {
@@ -112,47 +83,7 @@ namespace AlgorithmTests
                 }
             }
         }
-        [TestMethod]
-        public void FaceTestToFileTest ()
-        {
-            FaceTestToFile("dataDecember", "faces", 4, 2, 1, new double[] { 10, 5 });
-        }
-        [TestMethod]
-        public double FaceTestToFile(string dataset, string directory, int orientation, 
-            double stdx, double stdy, double[] lambda)
-        {
-            int finalMoment = 3;
-            string testComment = "";
-            int testFilesPerPerson = 2;
-            int trainFilesPerPerson = 4;
-            var MDC = new MinimumDistanceClassifier();
-            var extractor = new FaceFeatureExtractor(finalMoment, lambda, stdx, stdy, orientation);
-            TrainMDC(MDC, directory, trainFilesPerPerson, extractor, dataset);
-            bool firstRun = true;
-            int countSuccess = 0;
-            int countTotal = 0;
-            foreach (var dir in GetDirs(dataset))
-            {
-                for (int i=0; i < testFilesPerPerson; i++)
-                {
-                    var path = _sampleDirectory + dataset + @"\" + directory + @"\" + dir + @"\" + dir + (i + trainFilesPerPerson + 1) + ".bmp";
-                    if (File.Exists(path))
-                    {
-                        var featureVector = ExtractFeaturesFace(path, extractor);
-                        var result = MDC.Classify(featureVector);
-                        bool success = SaveFaceResult(result, extractor, dir, firstRun, 
-                            i + trainFilesPerPerson + 1, dataset, testComment);
-                        if (success)
-                        {
-                            countSuccess++;
-                        }
-                        countTotal++;
-                        firstRun = false;
-                    }
-                }
-            }
-            return SaveTotalResult(faceFilePath, countSuccess, countTotal);
-        }
+        
         [TestMethod]
         public double SpeechTestToFile(string dataset, string word, int filterBanks, int coeffs,
             float frameLength, float frameInterval, Window window)
@@ -189,26 +120,7 @@ namespace AlgorithmTests
             }
             return SaveTotalResult(speechFilePath, countSuccess, countTotal);
         }
-        private bool SaveFaceResult(string result, FaceFeatureExtractor extractor, string className, bool firstRun, int fileId, string dataset, string comment)
-        {
-            using (var resultFile = new StreamWriter(faceFilePath, true))
-            {
-                if (firstRun)
-                {
-                    resultFile.WriteLine("Date: " + DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToShortTimeString());
-                    resultFile.WriteLine("Used dataset: " + dataset);
-                    resultFile.WriteLine("Extractor params: ");
-                    resultFile.WriteLine("\t {0} : {1} , {2}", nameof(extractor.Lambda), extractor.Lambda[0], extractor.Lambda[1]);
-                    resultFile.WriteLine("\t {0} : {1}", nameof(extractor.Orientations), extractor.Orientations);
-                    resultFile.WriteLine("\t {0} : {1}", nameof(extractor.StdX), extractor.StdX);
-                    resultFile.WriteLine("\t {0} : {1}", nameof(extractor.StdY), extractor.StdY);
-                    resultFile.WriteLine("Comment: " + comment);
-                }
-                resultFile.WriteLine("Input file: {0}, input class: {1}, result class: {2}, {3}", className + fileId, className, result,
-                    result == className ? "SUCCESS" : "FAILURE");
-            }
-            return result == className;
-        }
+        
         private bool SaveSpeechResult(string result, SpeechFeatureExtractor extractor, FrameMaker frameMaker, string className,
             string word, bool firstRun, int fileId, string dataset, string comment)
         {
@@ -242,6 +154,7 @@ namespace AlgorithmTests
                 return percent;
             }
         }
+
         private void TrainDtw(DynamicTimeWarping dtw, string word, int trainFilesPerPerson, SpeechFeatureExtractor extractor,
             FrameMaker frameMaker, string dataset)
         {
@@ -261,30 +174,7 @@ namespace AlgorithmTests
             }
 
         }
-        private void TrainMDC(MinimumDistanceClassifier MDC, string directory, int trainFilesPerPerson, FaceFeatureExtractor extractor,
-            string dataset)
-        {
-            foreach (var dir in GetDirs(dataset))
-            {
-                var facevectors = new List<List<double>>();
-                for (int i = 0; i < trainFilesPerPerson; i++)
-                {
-                    if (File.Exists(_sampleDirectory + dataset + @"\" + directory + @"\" + dir + @"\" + dir + (i + 1) + ".bmp"))
-                    {
-                        var path = _sampleDirectory + dataset + @"\" + directory + @"\" + dir + @"\" + dir + (i + 1) + ".bmp";
-                        var featureVector = ExtractFeaturesFace(path, extractor);
-                        facevectors.Add(featureVector);
-                    }
-                }
-                MDC.AddToDictionary(facevectors,dir);
-            }
-        }
-        private List<double> ExtractFeaturesFace(string path, FaceFeatureExtractor extractor)
-        {
-            var file = new Bitmap(path);
-            return extractor.GetFeatureVector(file);
-        }
-
+        
         private List<double> ExtractFeaturesVoice(string path, SpeechFeatureExtractor extractor, FrameMaker frameMaker)
         {
             var file = WavReader.Read(path);
@@ -308,8 +198,5 @@ namespace AlgorithmTests
             public float FrameLength { get; set; }
             public List<double> Averages { get; set; }
         }
-
     }
-
-
 }
