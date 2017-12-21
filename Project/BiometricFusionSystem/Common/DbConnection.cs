08a23870e16bd85e78021a7e0a5e7c0c1ec94b13
slawinskam <m.slawinska@student.mini.pw.edu.pl>
2017-12-21 18:10:11 +0100

    connection string is generalized

--- Project/BiometricFusionSystem/Common/DbConnection.cs
+++ Project/BiometricFusionSystem/Common/DbConnection.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -27,8 +29,15 @@ namespace Common
         public DbConnection ()
         {
             _sqlConnection = new SqlConnection();
+            var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var databaseName = "biometricDB.mdf";
+            path = "\"" + path + "\\" + databaseName + "\"";
+
+            var path2 = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = \"C:\\Users\\aleks\\Desktop\\Engineering Thesis\\Project\\BiometricFusionSystem\\Common\\biometricDB.mdf\"; Integrated Security = True;";
             //external DBConnectionString = @"Data Source=DESKTOP-BDCV57Q\SQLEXPRESS;Initial Catalog=BiometricDB;Integrated Security=True";
-            ConnectionString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = \"C:\\Users\\aleks\\Desktop\\Engineering Thesis\\Project\\BiometricFusionSystem\\Common\\biometricDB.mdf\"; Integrated Security = True;";
+            //ConnectionString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = \"C:\\Users\\aleks\\Desktop\\Engineering Thesis\\Project\\BiometricFusionSystem\\Common\\biometricDB.mdf\"; Integrated Security = True;";
+            ConnectionString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = " + path + "; Integrated Security = True;";
+                
             _sqlConnection.ConnectionString = ConnectionString;
             _sqlConnection.FireInfoMessageEventOnUserErrors = false;
         }
